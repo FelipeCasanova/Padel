8e2081f5fe1c79cff5750ffb0de348ca813f6e56
unknown <felipe.casanova.ua@gmail.com>
2014-01-08 02:09:22 +0100

    Implemented new MachineKeySessionSecurityTokenHandler.

---
+++ Solutions/Padel.Infrastructure/Utilities/SessionSecurityToken/MachineKeySessionSecurityTokenHandler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.IdentityModel.Web;
+
+namespace Padel.Infrastructure.Utilities.SessionSecurityToken
+{
+
+    /// <summary>
+    /// This class encrypts the session security token using the ASP.NET configured machine key.
+    /// </summary>
+    public class MachineKeySessionSecurityTokenHandler : SessionSecurityTokenHandler
+    {
+        static List<CookieTransform> _transforms;
+
+        static MachineKeySessionSecurityTokenHandler()
+        {
+            _transforms = new List<CookieTransform>() 
+                        { 
+                            new DeflateCookieTransform(), 
+                            new MachineKeyProtectionTransform()
+                        };
+        }
+
+        public MachineKeySessionSecurityTokenHandler()
+            : base(_transforms.AsReadOnly())
+        {
+        }
+    }
+
+}
