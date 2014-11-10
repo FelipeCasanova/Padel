68cb32fc4deaed0e766294425040d6548d4e3dbc
unknown <felipe.casanova.ua@gmail.com>
2014-11-10 01:45:19 +0000

    Add field to the notification index.

--- Database/PadelDatabase/Schema Objects/Schemas/dbo/Tables/Indexes/Notificacions.IX_Cluster.sql
+++ Database/PadelDatabase/Schema Objects/Schemas/dbo/Tables/Indexes/Notificacions.IX_Cluster.sql
@@ -1,4 +1,4 @@
 ﻿-- =============================================
 -- Script Template
 -- =============================================
-CREATE UNIQUE CLUSTERED  INDEX Notificacions_Cluster_IX ON Notificacions(UsuarioId, FechaCreacion)
+CREATE UNIQUE CLUSTERED  INDEX Notificacions_Cluster_IX ON Notificacions(UsuarioId, FechaCreacion, NotificacionTipo)
