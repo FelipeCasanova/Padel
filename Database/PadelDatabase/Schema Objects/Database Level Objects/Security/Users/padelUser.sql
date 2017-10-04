6336220f22bcf73ae9ebdb14fbbe48161ea29a88
Felipe Casanova <felipe.casanova.ua@gmail.com>
2017-10-04 16:29:45 +0200

    Fixing NUnit dlls. Adding Roles to the database user.

--- Database/PadelDatabase/Schema Objects/Database Level Objects/Security/Users/padelUser.sql
+++ Database/PadelDatabase/Schema Objects/Database Level Objects/Security/Users/padelUser.sql
@@ -3,3 +3,6 @@
 -- =============================================
 CREATE USER [padel] FOR LOGIN [padel];
 ALTER ROLE [db_owner] ADD MEMBER [Padel];
+ALTER ROLE [db_accessadmin] ADD MEMBER [Padel];
+ALTER ROLE [db_datareader] ADD MEMBER [Padel];
+ALTER ROLE [db_datawriter] ADD MEMBER [Padel];
